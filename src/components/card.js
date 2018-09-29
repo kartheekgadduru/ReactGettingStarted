6ba4fdee9581f60f859f9afe015ac4ab0e49cad6
kartheekgadduru <kartheek.g1991@gmail.com>
2018-09-29 13:56:18 +0530

    third commit with props

--- src/components/card.js
+++ src/components/card.js
@@ -1,17 +1,12 @@
 const Card = props => {
   return (
     <div style={{ margin: "1em" }}>
-      <img
-        width="80"
-        src="https://avatars0.githubusercontent.com/u/1?v=4"
-        alt="kartheek"
-      />
+      <img width="80" src={props.avatar_url} alt="kartheek" />
       <div style={{ display: "inline-block", marginLeft: 10 }}>
         <div style={{ fortSize: "1.5em", fontWeight: "bold" }}>
-          {" "}
-          Name of the employee...
+          {props.name}
         </div>
-        <div> Company Name ...</div>
+        <div> {props.company}</div>
       </div>
     </div>
   );
@@ -20,6 +15,11 @@ const Card = props => {
 const CardList = props => {
   return (
     <div>
+      <Card
+        name="Tom Preston-Werner"
+        avatar_url="https://avatars0.githubusercontent.com/u/1?v=4"
+        company="null"
+      />
       <Card />
     </div>
   );
