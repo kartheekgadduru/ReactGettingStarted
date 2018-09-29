1ea34e9810f92d38026ced273ff5713110f99447
Kartheek <gadduru.kartheek@capgemini.com>
2018-09-29 13:32:21 +0530

    first commit cards

---
+++ src/components/card.js
@@ -0,0 +1,20 @@
+const Card = props => {
+  return (
+    <div style={{ margin: "1em" }}>
+      <img src="some value" alt="kartheek" />
+      <div style={{display:"inline-block", marginLeft:10}}>
+      <div style={{fortSize:"1.5em", fontWeight:"bold"}}> Name of the employee...</div>
+      <div> Company Name ...</div>
+      </div>
+    </div>
+  );
+};
+
+ReactDOM.render(<Card />, mountNode);
+
+const
+const CardList=(props)=>{
+  return(
+
+  )
+}
